a6b06494e232457453274f182cdad641e866d972
Asgeir Nilsen <asgeir.nilsen@miles.no>
2024-04-27 14:55:30 +0200

    1password terraform provider did not work.  autopilot cluster. Automatic kubectl configuration.

--- provider.tf
+++ provider.tf
@@ -15,18 +15,19 @@ terraform {
   required_version = ">= 0.12"
 }
 
-provider "onepassword" {
+#provider "onepassword" {
   # Configuration parameters such as subdomain, email, and secret key might go here.
   # Often environment variables are used for these to keep them out of code.
-}
+#}
 
-data "onepassword_item" "gcp_credentials" {
-  vault = "zpz2s4pe2j4a6s7ly3ndcmbonq"
-  title = "GCP Credentials"
-}
+#data "onepassword_item" "gcp_credentials" {
+#  vault = "zpz2s4pe2j4a6s7ly3ndcmbonq"
+#  title = "GCP Credentials"
+#}
 
 provider "google" {
-  credentials = jsondecode(base64decode(data.onepassword_item.gcp_credentials.documents[0].content))
+  #credentials = jsondecode(base64decode(data.onepassword_item.gcp_credentials.files[0].content))
+  credentials = file("credentials.json")
   project     = "asgeir"
   region      = "europe-north1"
 }
