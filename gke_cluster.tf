baad131629082395f5c77f8a30ab666799f6e21c
Asgeir Nilsen <asgeir.nilsen@miles.no>
2024-04-27 16:27:57 +0200

    Basic autopilot cluster.

--- gke_cluster.tf
+++ gke_cluster.tf
@@ -15,25 +15,3 @@ output "cluster_ca_certificate" {
   value = base64decode(google_container_cluster.autopilot_cluster.master_auth[0].cluster_ca_certificate)
   description = "The public certificate that is the root of trust for the cluster."
 }
-
-resource "null_resource" "update_kubectl" {
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-kubectl config set-cluster ${google_container_cluster.autopilot_cluster.name} --server=https://${self.triggers.cluster_endpoint} --certificate-authority=${self.triggers.cluster_ca_certificate} --embed-certs=true
-kubectl config set-credentials ${google_container_cluster.autopilot_cluster.name}-user --client-key=${self.triggers.client_key} --client-certificate=${self.triggers.client_certificate}
-kubectl config set-context ${google_container_cluster.autopilot_cluster.name} --cluster=${google_container_cluster.autopilot_cluster.name} --user=${google_container_cluster.autopilot_cluster.name}-user
-kubectl config use-context ${google_container_cluster.autopilot_cluster.name}
-EOT
-
-    environment = {
-      cluster_endpoint       = google_container_cluster.autopilot_cluster.endpoint
-      cluster_ca_certificate = base64decode(google_container_cluster.autopilot_cluster.master_auth[0].cluster_ca_certificate)
-      client_key             = base64decode(google_container_cluster.autopilot_cluster.master_auth[0].client_key)
-      client_certificate     = base64decode(google_container_cluster.autopilot_cluster.master_auth[0].client_certificate)
-    }
-  }
-}
--- provider.tf
+++ provider.tf
@@ -6,27 +6,12 @@ terraform {
       version = "~> 3.5"
     }
 
-    onepassword = {
-      source = "1Password/onepassword"
-      version = "~> 1.0"
-    }
   }
 
   required_version = ">= 0.12"
 }
 
-#provider "onepassword" {
-  # Configuration parameters such as subdomain, email, and secret key might go here.
-  # Often environment variables are used for these to keep them out of code.
-#}
-
-#data "onepassword_item" "gcp_credentials" {
-#  vault = "zpz2s4pe2j4a6s7ly3ndcmbonq"
-#  title = "GCP Credentials"
-#}
-
 provider "google" {
-  #credentials = jsondecode(base64decode(data.onepassword_item.gcp_credentials.files[0].content))
   credentials = file("credentials.json")
   project     = "asgeir"
   region      = "europe-north1"
